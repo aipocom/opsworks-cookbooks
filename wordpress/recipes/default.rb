e702dd19b47e66a92cb262f992d354da05d30752
takase <takase@aimluck.com>
2014-08-27 15:25:42 +0900

    test

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -6,23 +6,23 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
-include_recipe 'wordpress::core'
-
-execute "change permission" do
-  command <<-EOH    
-    chown nginx:nginx -R /var/www/vhosts/#{node[:wordpress][:instance_id]}
-  EOH
+node[:deploy].each do |app_name, deploy|
+  include_recipe 'wordpress::core'
+  
+  execute "change permission" do
+    command <<-EOH    
+      chown nginx:nginx -R /var/www/vhosts/#{node[:wordpress][:instance_id]}
+    EOH
+  end
+  
+  include_recipe "nginx::service"
+  
+  template "www.conf" do
+    path "/etc/nginx/conf.d/www.conf"
+    source "nginx-www.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    notifies :restart, "service[nginx]"
+  end
 end
-
-include_recipe "nginx::service"
-
-template "www.conf" do
-  path "/etc/nginx/conf.d/www.conf"
-  source "nginx-www.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  notifies :restart, "service[nginx]"
-end
-
